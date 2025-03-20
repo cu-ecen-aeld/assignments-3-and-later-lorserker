cc86aba29d50859597d2406f02dad1950d4102e9
Lorand <lorand.dali@52-entertainment.com>
2025-03-20 09:25:18 +0100

    assignment-4-part-1

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -14,6 +14,41 @@ void* threadfunc(void* thread_param)
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+
+    struct thread_data * thdata = (struct thread_data *)thread_param;
+
+    int err = 0;
+
+    err = usleep(thdata->wait_to_obtain_ms * 1000);
+    if (err) {
+        thdata->thread_complete_success = false;
+    }
+
+    if (!err) {
+        err = pthread_mutex_lock(thdata->mutex);
+        if (err) {
+            thdata->thread_complete_success = false;
+        }
+    }
+
+    if (!err) {
+        err = usleep(thdata->wait_to_release_ms * 1000);
+        if (err) {
+            thdata->thread_complete_success = false;
+        }
+    }
+
+    if (!err) {
+        err = pthread_mutex_unlock(thdata->mutex);
+        if (err) {
+            thdata->thread_complete_success = false;
+        }
+    }
+
+    if (!err) {
+        thdata->thread_complete_success = true;
+    }
+
     return thread_param;
 }
 
@@ -28,6 +63,27 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+
+    if (!thread || !mutex) {
+        return false;
+    }
+
+    struct thread_data * thdata = malloc(sizeof(struct thread_data));
+    if (!thdata) {
+        return false;
+    }
+
+    thdata->thread = thread;
+    thdata->mutex = mutex;
+    thdata->thread_complete_success = false;
+    thdata->wait_to_obtain_ms = wait_to_obtain_ms;
+    thdata->wait_to_release_ms = wait_to_release_ms;
+
+    int err = pthread_create(thdata->thread, NULL, threadfunc, thdata);
+    if (err) {
+        free(thdata);
+    }
+
+    return err == 0;
 }
 
--- examples/threading/threading.h
+++ examples/threading/threading.h
@@ -19,6 +19,14 @@ struct thread_data{
      * Set to true if the thread completed with success, false
      * if an error occurred.
      */
+
+    pthread_t *thread;
+
+    pthread_mutex_t *mutex;
+
+    int wait_to_obtain_ms;
+    int wait_to_release_ms;
+
     bool thread_complete_success;
 };
 
